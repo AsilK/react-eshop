f1ddf095d09bcaaf153ace4f1c9c3e60f997d108
AsilK <asil.karasu@hotmail.com>
2021-01-14 17:57:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <h2>HelloWorld</h2>
+      <div className="main">
+      <Header/>
+      <Homepage/>
+      </div>
     </div>
   );
 }
-
 export default App;
