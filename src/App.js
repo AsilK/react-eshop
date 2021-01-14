28f3bf9cca0320997a6fbb59f29575580eea00ce
AsilK <asil.karasu@hotmail.com>
2021-01-14 21:29:04 +0300

    react router layouts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import Header from './components/Header';
+
+import { BrowserRouter,Switch, Route } from 'react-router-dom';
+
+// layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+
+// pages
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="main">
-      <Header/>
-      <Homepage/>
-      </div>
+    
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => (
+          <HomepageLayout>
+            <Homepage />
+          </HomepageLayout>
+        )} />
+        <Route path="/registration" render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>
+        )} />
+      </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
+
 export default App;
