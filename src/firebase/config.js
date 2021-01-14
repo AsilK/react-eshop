975380d229a628974e182ed00efd522b15930b22
AsilK <asil.karasu@hotmail.com>
2021-01-14 23:23:29 +0300

    user auth

---
+++ src/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {apiKey: "AIzaSyC4LNQzIi7pQ-2zRbLVA0fzFPNxsQteVbU",
+authDomain: "asil-eshop.firebaseapp.com",
+projectId: "asil-eshop",
+storageBucket: "asil-eshop.appspot.com",
+messagingSenderId: "805522630566",
+appId: "1:805522630566:web:56151e312d7c1186f21167",
+measurementId: "G-FQM9J4S3VD"};
